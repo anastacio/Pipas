718083b3b07b1bf4eb99c1cf3cdd6f2d4a391b50
4n4st4cio <andreluiz.anastacio@gmail.com>
2019-03-12 21:15:53 +0000

    Add files via upload

---
+++ scripts/menuScene.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class menuScene : MonoBehaviour
+{
+
+	private CanvasGroup fadeGroup;
+	private float fadeInSpeed = 0.33f;
+
+ 
+    private void Start()
+    {
+        //grab the only CanvasGroup in the sccene
+        fadeGroup = FindObjectOfType<CanvasGroup>();
+
+        // Start with a white screen
+        fadeGroup.alpha = 1;
+        
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+    	// fade-in
+    	fadeGroup.alpha = 1 - Time.timeSinceLevelLoad * fadeInSpeed;
+    	
+        
+    }
+}
---
+++ scripts/preloader.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class preloader : MonoBehaviour
+{
+	private CanvasGroup fadeGroup;
+	private float loadTime;
+	private float minimumLogoTime = 3.0f; // tempo minimo da cena
+
+    private void Start()
+    {
+    	// grab only CanvasGroup in the scene
+    	fadeGroup = FindObjectOfType<CanvasGroup>();
+
+    	// começa com a tela branca
+    	fadeGroup.alpha = 1;
+
+    	// pre load do app
+
+    	//$$
+
+    	//marcação do time da cena
+
+
+    	// adequa o tempo de aparição do logo com o carregamento do app
+if(Time.time < minimumLogoTime)
+loadTime = minimumLogoTime;
+else
+loadTime = Time.time;
+        
+    }
+
+    private void Update()
+    {
+    	//fade - in
+if(Time.time < minimumLogoTime)
+{
+	fadeGroup.alpha = 1 - Time.time; 
+}
+
+    	//fade-out 
+
+if(Time.time > minimumLogoTime && loadTime != 0)
+{
+	fadeGroup.alpha = Time.time - minimumLogoTime;
+	if(fadeGroup.alpha >= 1)
+	{
+		//Debug.Log("muda a cena");
+        SceneManager.LoadScene("pipasmenu");
+	}
+    }
+}
+}
